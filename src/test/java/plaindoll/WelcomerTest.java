f2ea7013d2ef714e14b312a1d0e89b5d226cb65f
PavlenkoDm <dmpavlenko07@gmail.com>
2025-11-22 16:33:10 +0300

    add: sayScuf method and tests

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void welcomerSaysScuf(){
+		assertThat(welcomer.sayScuf(), containsString("hunter"));
+	}
 }
